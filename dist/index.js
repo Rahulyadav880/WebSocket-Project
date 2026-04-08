80c389816c6d95a3933cbdb702ac8f6a945dbe39
Rahul Yadav <ry9064526713@gmail.com>
2026-04-08 14:51:00 +0530

    Started a websocket simple chat app

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+let userCount = 0;
+//here socket is a reference to a socket which lets you to talk to the Client which has just 
+// connected to the websocket server, now the server can start receiving messages from the  
+// client on the socket and also can send messages using the same socket object
+wss.on("connection", (socket) => {
+    userCount += 1;
+    console.log("user connected # " + userCount);
+    //to send and receive the messages, we need a handler (socket.on is that handler)
+    socket.on("message", (msg) => {
+        console.log("message received " + msg.toString());
+        setTimeout(() => {
+            socket.send(msg.toString());
+        }, 1000);
+    });
+});
+//# sourceMappingURL=index.js.map
